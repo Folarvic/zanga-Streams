eb20a0ad29e3ca75ebc6c6454fa3871477e02f13
Samson <samfolarvic@gmail.com>
2025-05-01 18:28:37 +0100

    Songing

---
+++ src/components/song.ts
@@ -0,0 +1,30 @@
+const songs = [
+  {
+    id: 1, // Unique ID
+    title: "Morowore",
+    artist: "Olamide",
+    albumCover: "https://images.unsplash.com/photo-1649972904349-6e44c42644a7",
+    audioSrc: "/songs/Olamide-Morowore-(JustNaija.com).mp3",
+    startTime: 0, // start time in seconds
+    duration: 225, // 3 minutes 45 seconds converted to seconds
+  },
+  {
+    id: 2, // Unique ID
+    title: "Higher",
+    artist: "Burnaboy",
+    albumCover: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e",
+    audioSrc: "/songs/Burna-Boy-Higher-(JustNaija.com).mp3",
+    startTime: 0,
+    duration: 260, // 4 minutes 20 seconds converted to seconds
+  },
+  {
+    id: 3, // Unique ID
+    title: "If I’m Lying",
+    artist: "Burnaboy",
+    albumCover: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4",
+    audioSrc: "/songs/Burna-Boy-If-I-m-Lying-(JustNaija.com).mp3",
+    startTime: 0,
+    duration: 310, // 5 minutes 10 seconds converted to seconds
+  },
+];
+export default songs;
